15448481874ff4ff5f3ade2a72fafd1c06d25383
Kazuma Fujimaki <fujimaki-k@fujimakishouten.com>
2020-12-15 15:36:08 +0900

    Update config

--- cmd/hello/hello.go
+++ cmd/hello/hello.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
+	for _, argument := range os.Args {
+		if argument == "-v" || argument == "--version" {
+			fmt.Println("0.1.0")
+			os.Exit(0)
+		}
+	}
+
 	fmt.Println("Fight dayo!")
 }
